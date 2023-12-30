c39e011a589ae682da14e67d3e4863e2459d2530
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-31 12:32:59 +1300

    Adding server routing

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,3 +1,18 @@
 import express from 'express'
+import request from 'superagent'
+import 'dotenv/config'
 
-const router = express.Router()
+const exrouter = express.Router()
+
+exrouter.get('/api/v1/rates', async (req, res) => {
+  try {
+    const response = await request
+      .get('https://api.apilayer.com/exchangerates_data/latest')
+      .set('Authorization', `apikey ${process.env.CONVERT_CURR_KEY}`)
+    res.json(response.body)
+  } catch (error) {
+    res.sendStatus(500).json({ message: 'Unable to get rates' })
+  }
+})
+
+export default exrouter
