10f1e5542408ed28788ab7337e5a728e90204f62
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-31 13:25:36 +1300

    Adding server routing

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,8 @@ import { join } from 'node:path'
 import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-
+import request from 'superagent'
+import 'dotenv/config'
 import welcome from './routes/welcome.ts'
 import routes from './routes/routes.ts'
 
@@ -15,6 +16,16 @@ server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
 
 server.use('/api/v1/welcome', welcome)
-server.use('/api/v1/rates', routes)
+
+server.get('/api/v1/rates', async (req, res) => {
+  try {
+    const response = await request
+      .get('https://api.apilayer.com/exchangerates_data/latest?base=USD')
+      .set('apikey', `${process.env.CONVERT_CURR_KEY}`)
+    res.json(response.body)
+  } catch (error) {
+    res.sendStatus(500).json({ message: 'Unable to get rates' })
+  }
+})
 
 export default server
