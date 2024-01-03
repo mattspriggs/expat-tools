48a737269d2758cb238406c2f54c55762dc46b78
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-03 14:57:50 +1300

    Added utility functions to format currency and generate currency options

---
+++ client/utils/utils.js
@@ -0,0 +1,14 @@
+export function formatCurrency(amount, currency) {
+  return Intl.NumberFormat('en-US', { style: 'currency', currency }).format(
+    amount
+  )
+}
+
+export function generateOptions(options) {
+  return Object.entries(options)
+    .map(
+      ([currencyCode, currencyName]) =>
+        `<option value= "${currencyCode}">${currencyCode} - ${currencyName}</option>`
+    )
+    .join('')
+}
