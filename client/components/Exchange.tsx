268c83ea57e8b49395098f107e6245e04cb5b8fb
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-05 15:08:24 +1300

    Adding exchange component

--- client/components/Exchange.tsx
+++ client/components/Exchange.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react'
 import { convert } from '../src/convert.js'
 import { Exchange as ExchangeType } from '../../models/Exchange.ts'
+import { generateOptions } from '../utils/utils.js'
+import { currencies } from '../../models/currencies.js'
 
 export default function Exchange() {
+  const fromSelect = document.querySelector('[name="from_currency]"]')
+  const optionsHTML = generateOptions(currencies)
   return (
     <>
       <h2>Current Currency Exchange Rates</h2>
       <form>
         <input type="number" name="from_amount" />
         <select name="from_currency" id="">
-          <option>Select a Currency</option>
+          <option>{generateOptions(currencies)}</option>
         </select>
         <p>in</p>
         <select name="to_currency" id="">
