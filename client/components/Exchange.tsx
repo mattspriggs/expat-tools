cdf7d8f111de0dc68375583b4b545ed70a6f01f0
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-05 14:24:06 +1300

    Adding exchange component

--- client/components/Exchange.tsx
+++ client/components/Exchange.tsx
@@ -3,5 +3,19 @@ import { convert } from '../src/convert.js'
 import { Exchange as ExchangeType } from '../../models/Exchange.ts'
 
 export default function Exchange() {
-  return <h2>Exchange</h2>
+  return (
+    <>
+      <h2>Current Currency Exchange Rates</h2>
+      <form>
+        <input type="number" name="from_amount" />
+        <select name="from_currency" id="">
+          <option>Select a Currency</option>
+        </select>
+        <p>in</p>
+        <select name="to_currency" id="">
+          <option>Select a Currency</option>
+        </select>
+      </form>
+    </>
+  )
 }
