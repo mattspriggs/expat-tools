31d835447b04bccb2d85b220f749bfaeafe2a255
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-08 11:26:25 +1300

    Simplifying to use a fetch and use effect and state

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,5 +1,6 @@
 import request from 'superagent'
 import { Welcome } from '../models/welcome.ts'
+import { Exchange } from '../models/Exchange.ts'
 
 const serverURL = '/api/v1'
 
@@ -8,3 +9,7 @@ export function getWelcome(): Promise<Welcome> {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
 // ***   ***   ***
+export async function getRates(): Promise<Exchange[]> {
+  const response = await request.get(`${serverURL}/rates`)
+  return response.body
+}
--- client/components/Exchange.tsx
+++ client/components/Exchange.tsx
@@ -3,10 +3,30 @@ import { convert } from '../src/convert.js'
 import { Exchange as ExchangeType } from '../../models/Exchange.ts'
 import { generateOptions } from '../utils/utils.js'
 import { currencies } from '../../models/currencies.js'
+import { Currency } from '../../models/Currency.ts'
+import { getRates } from '../apiClient.ts'
 
 export default function Exchange() {
   const fromSelect = document.querySelector('[name="from_currency]"]')
   const optionsHTML = generateOptions(currencies)
+  const [exch, setExch] = useState<ExchangeType[] | null>(null)
+
+  async function fetchRates() {
+    const exchData = await getRates()
+    try {
+      setExch(exchData)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    try {
+      fetchRates()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
   return (
     <>
       <h2>Current Currency Exchange Rates</h2>
@@ -14,9 +34,9 @@ export default function Exchange() {
         <input type="number" name="from_amount" />
         <select name="from_currency" id="">
           <option>Select a Currency</option>
-          {currencies.map(
+          {exch?.map(
             (currency) =>
-              `<option value= "${currencyCode}">${currencyCode} - ${currencyName}</option>`
+              `<option value= "${currency.rates}">${currency.rates} - ${currency.rates}</option>`
           )}
         </select>
         <p>in</p>
