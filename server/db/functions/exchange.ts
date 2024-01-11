a154a45967069051a0a0a6fc16e239a5f2e15a27
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-12 11:50:36 +1300

    Added a get all function for excodes

--- server/db/functions/exchange.ts
+++ server/db/functions/exchange.ts
@@ -1,5 +1,6 @@
 import db from '../connection'
+import { Currency } from '../../../models/Currency.ts'
 
-export async function getExchangeCodes() {
+export async function getExchangeCodes(): Promise<Currency[]> {
   return await db('excodes').select('*')
 }
